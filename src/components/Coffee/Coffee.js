34018c5ac0563df381cb994dcc4edc73aed0e535
Sam Singer <samuel.singer1@gmail.com>
2018-10-05 14:46:01 -0700

    Mapping over the existing burger object

--- src/components/Coffee/Coffee.js
+++ src/components/Coffee/Coffee.js
@@ -3,11 +3,16 @@ import classes from './Coffee.css';
 import CoffeeIngredients from './CoffeeIngredients/CoffeeIngredients';
 
 const coffee = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <CoffeeIngredients key={igKey + i} type={igKey} />;
+            }) ;
+        }) 
     return (
         <div className={classes.Coffee}>
             <CoffeeIngredients type="bread-top" />
-            <CoffeeIngredients type="cheese" />
-            <CoffeeIngredients type="meat" />
+            { transformedIngredients }
             <CoffeeIngredients type="bread-bottom" />
         </div>
     );
--- src/containers/CoffeeBuilder/CoffeeBuilder.js
+++ src/containers/CoffeeBuilder/CoffeeBuilder.js
@@ -3,10 +3,20 @@ import Aux from '../../hoc/Aux';
 import Coffee from '../../components/Coffee/Coffee';
 
 class CoffeeBuilder extends Component {
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    }
+
+
     render() {
         return(
             <Aux>
-                <Coffee />
+                <Coffee ingredients={this.state.ingredients}/>
                 <div>Build Controls</div>
             </Aux>
         );
