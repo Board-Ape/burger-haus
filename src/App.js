1670fd24899aa17170d7b88081a4e9bfa83fba89
Sam Singer <samuel.singer1@gmail.com>
2018-10-05 13:13:06 -0700

    Creating a coffee builder component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout/Layout';
+import CoffeeBuilder from './containers/CoffeeBuilder/CoffeeBuilder';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <p>Test</p>
+          <CoffeeBuilder /> 
         </Layout>
       </div>
     );
