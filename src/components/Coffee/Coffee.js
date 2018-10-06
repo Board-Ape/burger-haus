519bb38ba80633ce15690d68112c4fd43233e4c8
Sam Singer <samuel.singer1@gmail.com>
2018-10-05 17:18:39 -0700

    Now rendering a more dynamic burger with no starting ingredients

--- src/components/Coffee/Coffee.js
+++ src/components/Coffee/Coffee.js
@@ -3,12 +3,21 @@ import classes from './Coffee.css';
 import CoffeeIngredients from './CoffeeIngredients/CoffeeIngredients';
 
 const coffee = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
+
                 return <CoffeeIngredients key={igKey + i} type={igKey} />;
             }) ;
         }) 
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        },[]);
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please Start Adding Ingredients</p>
+    }
     return (
         <div className={classes.Coffee}>
             <CoffeeIngredients type="bread-top" />
