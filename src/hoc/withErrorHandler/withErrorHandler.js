74550d73d32c73b3cbfae2b66205ced3cf3737bf
Sam Singer <samuel.singer1@gmail.com>
2018-10-19 13:22:16 -0700

    Clean up testing code

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -21,7 +21,6 @@ const withErrrorHandler = ( WrappedComponent, axios ) => {
         }
 
         componentWillUnmount () {
-            console.log('Will Unmount', this.reqInterceptor, this.resInterceptor)
             axios.interceptors.request.eject(this.reqInterceptor);
             axios.interceptors.response.eject(this.resInterceptor);
         }
